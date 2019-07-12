3e4c9b7fdd10a3aa6212f9b361af2de1a9ffdcc8
ArtemThaplya <artem.thaplya@gmail.com>
2019-07-12 10:56:43 +0300

    spring boot dependency, random state

---
+++ src/main/java/com/tsaplya/web/Main.java
@@ -0,0 +1,10 @@
+package com.tsaplya.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
