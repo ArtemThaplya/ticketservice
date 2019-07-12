299ef98e02d25dd662d13a1abf6ecadaf7dcc557
ArtemThaplya <artem.thaplya@gmail.com>
2019-07-12 14:47:32 +0300

    add integration test

---
+++ src/test/java/com/tsaplya/web/controller/PaymentControllerTest.java
@@ -0,0 +1,35 @@
+package com.tsaplya.web.controller;
+
+import com.tsaplya.web.model.State;
+import com.tsaplya.web.service.PaymentService;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.tsaplya.web.model.State.DONE;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PaymentController.class)
+public class PaymentControllerTest {
+
+    @Autowired
+    private PaymentController service;
+
+    @MockBean
+    PaymentService paymentServiceMock;
+
+    @Test
+    public void test() {
+        when(paymentServiceMock.getSate()).thenReturn(DONE);
+
+        State paymentState = service.payment();
+
+        assertThat(paymentState, Is.is(DONE));
+    }
+}
